25d7527ac8f9ea8df4793b9f9279b55c4b6de001
Pedro Ladaria <pedro.ladaria@gmail.com>
2023-05-08 23:59:12 +0200

    copy source files into megadrive project using hardlinks

---
+++ megadrive/prepare.sh
@@ -0,0 +1,15 @@
+# copy using hardlinks
+cp -al ../src/* src/supaplex
+
+# remove unneeded files
+rm -rf src/supaplex/3ds \
+    src/supaplex/apple \
+    src/supaplex/nds \
+    src/supaplex/nx \
+    src/supaplex/ps3 \
+    src/supaplex/ps2 \
+    src/supaplex/psp \
+    src/supaplex/psp2 \
+    src/supaplex/sdl2 \
+    src/supaplex/wii \
+    src/supaplex/wiiu
---
+++ megadrive/src/SDL/SDL.h
@@ -0,0 +1,4 @@
+#ifndef _SDL_H
+#define _SDL_H 1
+
+#endif
